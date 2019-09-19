509d587ce276c15a39dd29ca9c6afb978ef5f085
169000707zhang <447613512@qq.com>
2019-09-19 11:17:05 +0800

    张胜   2019.9.19

--- evaluation/src/main/java/evaluation/controller/TeacherController.java
+++ evaluation/src/main/java/evaluation/controller/TeacherController.java
@@ -1,10 +1,41 @@
 package evaluation.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import evaluation.entity.Teacher;
+import evaluation.service.TeacherService;
+
+
 
 @Controller
 @RequestMapping("/teacher")
 public class TeacherController {
 
+	@Autowired
+	private TeacherService teacherService;
+        
+	 @RequestMapping("/login")
+     public ModelAndView login() {
+    	 ModelAndView mv=new ModelAndView("teacher/login");
+    	 return mv;
+}
+	 
+	 @RequestMapping("/managerlogin")
+	 public ModelAndView  managerlogin(Model model,Teacher teacher){
+		 model.addAttribute("teacher",teacher);
+		 ModelAndView mv=new ModelAndView("teacher/managerlogin");
+		 ModelAndView mv2=new ModelAndView("teacher/error");
+		 boolean isLogin=teacherService.login(teacher.getTeachernumber(),teacher.getPassword());
+		 if(isLogin) {
+			 System.out.println("hello");
+			 return mv;
+		 }
+		return mv2;
+		
+		
+	 }
+	 
 }
--- evaluation/src/main/java/evaluation/dao/FacultyDao.java
+++ evaluation/src/main/java/evaluation/dao/FacultyDao.java
@@ -1,8 +1,15 @@
 package evaluation.dao;
 
+import java.util.List;
 import evaluation.entity.Faculty;
 
 public interface FacultyDao {
-	Faculty getFaculty(String facultyid);
-
+	int addFaculty(Faculty faculty);
+	int delFaculty(int facultyid);
+	int updateFaculty(Faculty faculty);
+	List<Faculty> getFaculty();
+	int isFaculty(int facultyid);
+	Faculty getFacultybyId(int facultyid);
+	int isFacultybyname(String facultyname);
+	int delchecked(String[] aa);
 }
